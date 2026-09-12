849532d713dbe402b6ecd807f60105c93613c597
aliyatang <aliya.w.tang@gmail.com>
2026-09-11 18:08:33 -0700

    Integrate Sutter into multi-system warehouse

--- sql/09_build_multisystem_price_fact.sql
+++ sql/09_build_multisystem_price_fact.sql
@@ -8,11 +8,11 @@ AS
 -- ============================================================
 
 SELECT
-  system_name,
-  facility_id,
-  mrf_location_name,
+  CAST(system_name AS STRING) AS system_name,
+  CAST(facility_id AS STRING) AS facility_id,
+  CAST(mrf_location_name AS STRING) AS mrf_location_name,
 
-  description,
+  CAST(description AS STRING) AS description,
   CAST(NULL AS STRING) AS billing_class,
 
   CAST(code_1 AS STRING) AS code_1,
@@ -21,54 +21,54 @@ SELECT
   CAST(code_2_type AS STRING) AS code_2_type,
   CAST(NULL AS STRING) AS all_codes_json,
 
-  modifier,
-  modifiers,
+  CAST(modifier AS STRING) AS modifier,
+  CAST(modifiers AS STRING) AS modifiers,
 
   CAST(drug_unit_of_measurement AS STRING) AS drug_unit,
-  drug_type_of_measurement AS drug_type,
+  CAST(drug_type_of_measurement AS STRING) AS drug_type,
 
-  setting,
+  CAST(setting AS STRING) AS setting,
 
-  gross_charge,
-  discounted_cash_charge,
-  minimum_charge,
-  maximum_charge,
+  CAST(gross_charge AS FLOAT64) AS gross_charge,
+  CAST(discounted_cash_charge AS FLOAT64) AS discounted_cash_charge,
+  CAST(minimum_charge AS FLOAT64) AS minimum_charge,
+  CAST(maximum_charge AS FLOAT64) AS maximum_charge,
 
-  additional_generic_notes,
+  CAST(additional_generic_notes AS STRING) AS additional_generic_notes,
 
-  payer,
-  plan,
+  CAST(payer AS STRING) AS payer,
+  CAST(plan AS STRING) AS plan,
 
-  negotiated_dollar,
-  negotiated_percentage,
-  negotiated_algorithm,
+  CAST(negotiated_dollar AS FLOAT64) AS negotiated_dollar,
+  CAST(negotiated_percentage AS FLOAT64) AS negotiated_percentage,
+  CAST(negotiated_algorithm AS STRING) AS negotiated_algorithm,
 
-  median_amount,
-  p10_amount,
-  p90_amount,
+  CAST(median_amount AS FLOAT64) AS median_amount,
+  CAST(p10_amount AS FLOAT64) AS p10_amount,
+  CAST(p90_amount AS FLOAT64) AS p90_amount,
 
-  negotiated_count,
-  methodology,
-  additional_payer_notes,
+  CAST(negotiated_count AS STRING) AS negotiated_count,
+  CAST(methodology AS STRING) AS methodology,
+  CAST(additional_payer_notes AS STRING) AS additional_payer_notes,
 
-  has_negotiated_dollar,
-  has_negotiated_percentage,
-  has_negotiated_algorithm,
-  has_negotiated_price,
+  CAST(has_negotiated_dollar AS BOOL) AS has_negotiated_dollar,
+  CAST(has_negotiated_percentage AS BOOL) AS has_negotiated_percentage,
+  CAST(has_negotiated_algorithm AS BOOL) AS has_negotiated_algorithm,
+  CAST(has_negotiated_price AS BOOL) AS has_negotiated_price,
 
-  price_representation,
+  CAST(price_representation AS STRING) AS price_representation,
 
-  'CSV' AS source_format,
+  CAST('CSV' AS STRING) AS source_format,
 
-  source_row_number,
+  CAST(source_row_number AS INT64) AS source_row_number,
 
   CAST(NULL AS INT64) AS source_service_number,
   CAST(NULL AS INT64) AS source_charge_number,
   CAST(NULL AS INT64) AS source_payer_number,
   CAST(NULL AS STRING) AS source_record_id,
 
-  mrf_url,
-  source_page_url,
+  CAST(mrf_url AS STRING) AS mrf_url,
+  CAST(source_page_url AS STRING) AS source_page_url,
 
   CAST(NULL AS STRING) AS mrf_hospital_name,
   CAST(NULL AS STRING) AS mrf_address,
@@ -90,11 +90,11 @@ UNION ALL
 -- ============================================================
 
 SELECT
-  system_name,
-  facility_id,
-  mrf_location_name,
+  CAST(system_name AS STRING) AS system_name,
+  CAST(facility_id AS STRING) AS facility_id,
+  CAST(mrf_location_name AS STRING) AS mrf_location_name,
 
-  description,
+  CAST(description AS STRING) AS description,
   CAST(NULL AS STRING) AS billing_class,
 
   CAST(code_1 AS STRING) AS code_1,
@@ -103,62 +103,62 @@ SELECT
   CAST(code_2_type AS STRING) AS code_2_type,
   CAST(all_codes_json AS STRING) AS all_codes_json,
 
-  modifier,
-  modifiers,
+  CAST(modifier AS STRING) AS modifier,
+  CAST(modifiers AS STRING) AS modifiers,
 
   CAST(drug_unit AS STRING) AS drug_unit,
-  drug_type,
+  CAST(drug_type AS STRING) AS drug_type,
 
-  setting,
+  CAST(setting AS STRING) AS setting,
 
-  gross_charge,
-  discounted_cash_charge,
-  minimum_charge,
-  maximum_charge,
+  CAST(gross_charge AS FLOAT64) AS gross_charge,
+  CAST(discounted_cash_charge AS FLOAT64) AS discounted_cash_charge,
+  CAST(minimum_charge AS FLOAT64) AS minimum_charge,
+  CAST(maximum_charge AS FLOAT64) AS maximum_charge,
 
-  additional_generic_notes,
+  CAST(additional_generic_notes AS STRING) AS additional_generic_notes,
 
-  payer,
-  plan,
+  CAST(payer AS STRING) AS payer,
+  CAST(plan AS STRING) AS plan,
 
-  negotiated_dollar,
-  negotiated_percentage,
-  negotiated_algorithm,
+  CAST(negotiated_dollar AS FLOAT64) AS negotiated_dollar,
+  CAST(negotiated_percentage AS FLOAT64) AS negotiated_percentage,
+  CAST(negotiated_algorithm AS STRING) AS negotiated_algorithm,
 
-  median_amount,
-  p10_amount,
-  p90_amount,
+  CAST(median_amount AS FLOAT64) AS median_amount,
+  CAST(p10_amount AS FLOAT64) AS p10_amount,
+  CAST(p90_amount AS FLOAT64) AS p90_amount,
 
-  negotiated_count,
-  methodology,
-  additional_payer_notes,
+  CAST(negotiated_count AS STRING) AS negotiated_count,
+  CAST(methodology AS STRING) AS methodology,
+  CAST(additional_payer_notes AS STRING) AS additional_payer_notes,
 
-  has_negotiated_dollar,
-  has_negotiated_percentage,
-  has_negotiated_algorithm,
-  has_negotiated_price,
+  CAST(has_negotiated_dollar AS BOOL) AS has_negotiated_dollar,
+  CAST(has_negotiated_percentage AS BOOL) AS has_negotiated_percentage,
+  CAST(has_negotiated_algorithm AS BOOL) AS has_negotiated_algorithm,
+  CAST(has_negotiated_price AS BOOL) AS has_negotiated_price,
 
-  price_representation,
+  CAST(price_representation AS STRING) AS price_representation,
 
-  'JSON' AS source_format,
+  CAST('JSON' AS STRING) AS source_format,
 
   CAST(NULL AS INT64) AS source_row_number,
 
-  source_service_number,
-  source_charge_number,
-  source_payer_number,
-  source_record_id,
+  CAST(source_service_number AS INT64) AS source_service_number,
+  CAST(source_charge_number AS INT64) AS source_charge_number,
+  CAST(source_payer_number AS INT64) AS source_payer_number,
+  CAST(source_record_id AS STRING) AS source_record_id,
 
   CAST(NULL AS STRING) AS mrf_url,
   CAST(NULL AS STRING) AS source_page_url,
 
-  mrf_hospital_name,
-  mrf_address,
-  mrf_license_number,
-  mrf_type_2_npi,
-  mrf_last_updated_on,
-  mrf_as_of_date,
-  mrf_version
+  CAST(mrf_hospital_name AS STRING) AS mrf_hospital_name,
+  CAST(mrf_address AS STRING) AS mrf_address,
+  CAST(mrf_license_number AS STRING) AS mrf_license_number,
+  CAST(mrf_type_2_npi AS STRING) AS mrf_type_2_npi,
+  CAST(mrf_last_updated_on AS STRING) AS mrf_last_updated_on,
+  CAST(mrf_as_of_date AS STRING) AS mrf_as_of_date,
+  CAST(mrf_version AS STRING) AS mrf_version
 
 FROM
   `california-hospital-pricing.hospital_pricing.fact_ucla_negotiated_price`
@@ -240,9 +240,7 @@ SELECT
   CAST(mrf_license_number AS STRING) AS mrf_license_number,
   CAST(mrf_type_2_npi AS STRING) AS mrf_type_2_npi,
   CAST(mrf_last_updated_on AS STRING) AS mrf_last_updated_on,
-
   CAST(NULL AS STRING) AS mrf_as_of_date,
-
   CAST(mrf_version AS STRING) AS mrf_version
 
 FROM
--- sql/10_build_hospital_location_dimension.sql
+++ sql/10_build_hospital_location_dimension.sql
@@ -9,76 +9,71 @@ AS
 
 -- ============================================================
 -- Kaiser Permanente
--- One row per actual MRF location/campus
 -- ============================================================
 
 SELECT
-  'Kaiser Permanente' AS system_name,
+  CAST('Kaiser Permanente' AS STRING) AS system_name,
 
-  k.facility_id,
-  k.mrf_location_name,
+  CAST(k.facility_id AS STRING) AS facility_id,
+  CAST(k.mrf_location_name AS STRING) AS mrf_location_name,
 
-  -- MRF identity
-  k.mrf_hospital_name,
-  k.mrf_address,
-  k.mrf_license_number,
-  k.mrf_type_2_npi,
+  CAST(k.mrf_hospital_name AS STRING) AS mrf_hospital_name,
+  CAST(k.mrf_address AS STRING) AS mrf_address,
+  CAST(k.mrf_license_number AS STRING) AS mrf_license_number,
+  CAST(k.mrf_type_2_npi AS STRING) AS mrf_type_2_npi,
 
   CAST(NULL AS STRING) AS mrf_last_updated_on,
   CAST(NULL AS STRING) AS mrf_as_of_date,
   CAST(NULL AS STRING) AS mrf_version,
 
-  -- CMS / general hospital identity
-  f.cms_facility_name AS facility_name,
+  CAST(f.cms_facility_name AS STRING) AS facility_name,
 
-  CAST(NULL AS STRING) AS address,
-  k.hcai_city AS city,
-  'CA' AS state,
-  k.hcai_zip AS zip_code,
+  CAST(k.hcai_address_dim AS STRING) AS address,
+  CAST(k.hcai_city AS STRING) AS city,
+  CAST('CA' AS STRING) AS state,
+  CAST(k.hcai_zip AS STRING) AS zip_code,
+
   CAST(NULL AS STRING) AS county,
   CAST(NULL AS STRING) AS telephone_number,
 
-  f.hospital_type,
-  f.hospital_ownership,
-  f.emergency_services,
-  f.overall_rating,
+  CAST(f.hospital_type AS STRING) AS hospital_type,
+  CAST(f.hospital_ownership AS STRING) AS hospital_ownership,
+  CAST(f.emergency_services AS STRING) AS emergency_services,
+  CAST(f.overall_rating AS FLOAT64) AS overall_rating,
 
-  TRUE AS analysis_eligible,
+  CAST(TRUE AS BOOL) AS analysis_eligible,
 
-  -- HCAI
-  k.hcai_id,
-  k.hcai_facility_name_dim AS hcai_facility_name,
-  k.hcai_address_dim AS hcai_address,
-  k.hcai_city,
-  k.hcai_zip AS hcai_zip_code,
+  CAST(k.hcai_id AS STRING) AS hcai_id,
+  CAST(k.hcai_facility_name_dim AS STRING) AS hcai_facility_name,
+  CAST(k.hcai_address_dim AS STRING) AS hcai_address,
+  CAST(k.hcai_city AS STRING) AS hcai_city,
+  CAST(k.hcai_zip AS STRING) AS hcai_zip_code,
 
-  k.longitude,
-  k.latitude,
+  CAST(k.longitude AS FLOAT64) AS longitude,
+  CAST(k.latitude AS FLOAT64) AS latitude,
 
-  k.license_type,
-  k.license_category,
-  k.facility_level,
-  k.er_service_level,
-  k.licensed_beds,
-  k.facility_status,
+  CAST(k.license_type AS STRING) AS license_type,
+  CAST(k.license_category AS STRING) AS license_category,
+  CAST(k.facility_level AS STRING) AS facility_level,
+  CAST(k.er_service_level AS STRING) AS er_service_level,
+  CAST(k.licensed_beds AS FLOAT64) AS licensed_beds,
+  CAST(k.facility_status AS STRING) AS facility_status,
 
-  -- Matching
-  k.match_method,
-  k.combined_score AS match_score,
+  CAST(k.match_method AS STRING) AS match_method,
+  CAST(k.combined_score AS FLOAT64) AS match_score,
 
-  -- ACS / ZCTA
-  k.zcta AS census_zcta,
-  k.zcta_name AS census_zcta_name,
-  k.population AS zcta_population,
-  k.median_household_income AS zcta_median_household_income,
-  k.poverty_rate AS zcta_poverty_rate,
-  k.uninsured_rate AS zcta_uninsured_rate,
-  k.unemployment_rate AS zcta_unemployment_rate,
-  k.bachelors_or_higher_rate AS zcta_bachelors_or_higher_rate,
+  CAST(k.zcta AS STRING) AS census_zcta,
+  CAST(k.zcta_name AS STRING) AS census_zcta_name,
+  CAST(k.population AS FLOAT64) AS zcta_population,
+  CAST(k.median_household_income AS FLOAT64) AS zcta_median_household_income,
+  CAST(k.poverty_rate AS FLOAT64) AS zcta_poverty_rate,
+  CAST(k.uninsured_rate AS FLOAT64) AS zcta_uninsured_rate,
+  CAST(k.unemployment_rate AS FLOAT64) AS zcta_unemployment_rate,
+  CAST(k.bachelors_or_higher_rate AS FLOAT64) AS zcta_bachelors_or_higher_rate,
 
   CAST(NULL AS STRING) AS zcta_match_method,
 
-  k.zcta IS NOT NULL AS census_acs_matched
+  CAST(k.zcta IS NOT NULL AS BOOL) AS census_acs_matched
 
 FROM
   `california-hospital-pricing.hospital_pricing.dim_kaiser_location` k
@@ -91,11 +86,14 @@ LEFT JOIN (
     ANY_VALUE(hospital_ownership) AS hospital_ownership,
     ANY_VALUE(emergency_services) AS emergency_services,
     ANY_VALUE(overall_rating) AS overall_rating
+
   FROM
     `california-hospital-pricing.hospital_pricing.fact_kaiser_negotiated_price`
+
   GROUP BY
     facility_id
 ) f
+
 USING (facility_id)
 
 
@@ -104,73 +102,68 @@ UNION ALL
 
 -- ============================================================
 -- UCLA Health
--- One eligible MRF location per CMS facility
 -- ============================================================
 
 SELECT
-  u.system_name,
+  CAST(u.system_name AS STRING) AS system_name,
 
-  u.facility_id,
-  m.mrf_location_name,
+  CAST(u.facility_id AS STRING) AS facility_id,
+  CAST(m.mrf_location_name AS STRING) AS mrf_location_name,
 
-  -- MRF identity
-  m.mrf_hospital_name,
-  m.mrf_address,
-  m.mrf_license_number,
-  m.mrf_type_2_npi,
-  m.mrf_last_updated_on,
-  m.mrf_as_of_date,
-  m.mrf_version,
+  CAST(m.mrf_hospital_name AS STRING) AS mrf_hospital_name,
+  CAST(m.mrf_address AS STRING) AS mrf_address,
+  CAST(m.mrf_license_number AS STRING) AS mrf_license_number,
+  CAST(m.mrf_type_2_npi AS STRING) AS mrf_type_2_npi,
+  CAST(m.mrf_last_updated_on AS STRING) AS mrf_last_updated_on,
+  CAST(m.mrf_as_of_date AS STRING) AS mrf_as_of_date,
+  CAST(m.mrf_version AS STRING) AS mrf_version,
 
-  -- CMS / general hospital identity
-  u.facility_name,
-  u.address,
-  u.city,
-  u.state,
-  u.zip_code,
-  u.county,
-  u.telephone_number,
+  CAST(u.facility_name AS STRING) AS facility_name,
+  CAST(u.address AS STRING) AS address,
+  CAST(u.city AS STRING) AS city,
+  CAST(u.state AS STRING) AS state,
+  CAST(u.zip_code AS STRING) AS zip_code,
+  CAST(u.county AS STRING) AS county,
+  CAST(u.telephone_number AS STRING) AS telephone_number,
 
-  u.hospital_type,
-  u.hospital_ownership,
-  u.emergency_services,
-  u.overall_rating,
+  CAST(u.hospital_type AS STRING) AS hospital_type,
+  CAST(u.hospital_ownership AS STRING) AS hospital_ownership,
+  CAST(u.emergency_services AS STRING) AS emergency_services,
+  CAST(u.overall_rating AS FLOAT64) AS overall_rating,
 
-  u.analysis_eligible,
+  CAST(u.analysis_eligible AS BOOL) AS analysis_eligible,
 
-  -- HCAI
-  u.hcai_id,
-  u.hcai_facility_name,
-  u.hcai_address,
-  u.hcai_city,
-  u.hcai_zip_code,
+  CAST(u.hcai_id AS STRING) AS hcai_id,
+  CAST(u.hcai_facility_name AS STRING) AS hcai_facility_name,
+  CAST(u.hcai_address AS STRING) AS hcai_address,
+  CAST(u.hcai_city AS STRING) AS hcai_city,
+  CAST(u.hcai_zip_code AS STRING) AS hcai_zip_code,
 
-  u.longitude,
-  u.latitude,
+  CAST(u.longitude AS FLOAT64) AS longitude,
+  CAST(u.latitude AS FLOAT64) AS latitude,
 
   CAST(NULL AS STRING) AS license_type,
-  u.license_category,
-  u.facility_level,
-  u.er_service_level,
-  u.licensed_beds,
-  u.facility_status,
+  CAST(u.license_category AS STRING) AS license_category,
+  CAST(u.facility_level AS STRING) AS facility_level,
+  CAST(u.er_service_level AS STRING) AS er_service_level,
+  CAST(u.licensed_beds AS FLOAT64) AS licensed_beds,
+  CAST(u.facility_status AS STRING) AS facility_status,
 
-  -- Matching
-  u.match_method,
-  u.match_score,
+  CAST(u.match_method AS STRING) AS match_method,
+  CAST(u.match_score AS FLOAT64) AS match_score,
 
-  -- ACS / ZCTA
-  u.census_zcta,
-  u.census_zcta_name,
-  u.zcta_population,
-  u.zcta_median_household_income,
-  u.zcta_poverty_rate,
-  u.zcta_uninsured_rate,
-  u.zcta_unemployment_rate,
-  u.zcta_bachelors_or_higher_rate,
+  CAST(u.census_zcta AS STRING) AS census_zcta,
+  CAST(u.census_zcta_name AS STRING) AS census_zcta_name,
+  CAST(u.zcta_population AS FLOAT64) AS zcta_population,
+  CAST(u.zcta_median_household_income AS FLOAT64) AS zcta_median_household_income,
+  CAST(u.zcta_poverty_rate AS FLOAT64) AS zcta_poverty_rate,
+  CAST(u.zcta_uninsured_rate AS FLOAT64) AS zcta_uninsured_rate,
+  CAST(u.zcta_unemployment_rate AS FLOAT64) AS zcta_unemployment_rate,
+  CAST(u.zcta_bachelors_or_higher_rate AS FLOAT64) AS zcta_bachelors_or_higher_rate,
 
-  u.zcta_match_method,
-  u.census_acs_matched
+  CAST(u.zcta_match_method AS STRING) AS zcta_match_method,
+
+  CAST(u.census_acs_matched AS BOOL) AS census_acs_matched
 
 FROM
   `california-hospital-pricing.hospital_pricing.dim_ucla_location` u
@@ -178,8 +171,8 @@ FROM
 LEFT JOIN (
   SELECT
     facility_id,
-    mrf_location_name,
 
+    ANY_VALUE(mrf_location_name) AS mrf_location_name,
     ANY_VALUE(mrf_hospital_name) AS mrf_hospital_name,
     ANY_VALUE(mrf_address) AS mrf_address,
     ANY_VALUE(mrf_license_number) AS mrf_license_number,
@@ -192,7 +185,100 @@ LEFT JOIN (
     `california-hospital-pricing.hospital_pricing.fact_ucla_negotiated_price`
 
   GROUP BY
-    facility_id,
-    mrf_location_name
+    facility_id
 ) m
+
+USING (facility_id)
+
+
+UNION ALL
+
+
+-- ============================================================
+-- Sutter Health
+-- ============================================================
+
+SELECT
+  CAST(s.system_name AS STRING) AS system_name,
+
+  CAST(s.facility_id AS STRING) AS facility_id,
+  CAST(m.mrf_location_name AS STRING) AS mrf_location_name,
+
+  CAST(m.mrf_hospital_name AS STRING) AS mrf_hospital_name,
+  CAST(m.mrf_address AS STRING) AS mrf_address,
+  CAST(m.mrf_license_number AS STRING) AS mrf_license_number,
+  CAST(m.mrf_type_2_npi AS STRING) AS mrf_type_2_npi,
+  CAST(m.mrf_last_updated_on AS STRING) AS mrf_last_updated_on,
+  CAST(NULL AS STRING) AS mrf_as_of_date,
+  CAST(m.mrf_version AS STRING) AS mrf_version,
+
+  CAST(s.facility_name AS STRING) AS facility_name,
+  CAST(s.address AS STRING) AS address,
+  CAST(s.city AS STRING) AS city,
+  CAST(s.state AS STRING) AS state,
+  CAST(s.zip_code AS STRING) AS zip_code,
+  CAST(s.county AS STRING) AS county,
+  CAST(s.telephone_number AS STRING) AS telephone_number,
+
+  CAST(s.hospital_type AS STRING) AS hospital_type,
+  CAST(s.hospital_ownership AS STRING) AS hospital_ownership,
+  CAST(s.emergency_services AS STRING) AS emergency_services,
+  CAST(s.overall_rating AS FLOAT64) AS overall_rating,
+
+  CAST(s.analysis_eligible AS BOOL) AS analysis_eligible,
+
+  CAST(s.hcai_id AS STRING) AS hcai_id,
+  CAST(s.hcai_facility_name AS STRING) AS hcai_facility_name,
+  CAST(s.hcai_address AS STRING) AS hcai_address,
+  CAST(s.hcai_city AS STRING) AS hcai_city,
+  CAST(s.hcai_zip_code AS STRING) AS hcai_zip_code,
+
+  CAST(s.longitude AS FLOAT64) AS longitude,
+  CAST(s.latitude AS FLOAT64) AS latitude,
+
+  CAST(NULL AS STRING) AS license_type,
+  CAST(s.license_category AS STRING) AS license_category,
+  CAST(s.facility_level AS STRING) AS facility_level,
+  CAST(s.er_service_level AS STRING) AS er_service_level,
+  CAST(s.licensed_beds AS FLOAT64) AS licensed_beds,
+  CAST(s.facility_status AS STRING) AS facility_status,
+
+  CAST(s.match_method AS STRING) AS match_method,
+  CAST(s.match_score AS FLOAT64) AS match_score,
+
+  CAST(s.census_zcta AS STRING) AS census_zcta,
+  CAST(s.census_zcta_name AS STRING) AS census_zcta_name,
+  CAST(s.zcta_population AS FLOAT64) AS zcta_population,
+  CAST(s.zcta_median_household_income AS FLOAT64) AS zcta_median_household_income,
+  CAST(s.zcta_poverty_rate AS FLOAT64) AS zcta_poverty_rate,
+  CAST(s.zcta_uninsured_rate AS FLOAT64) AS zcta_uninsured_rate,
+  CAST(s.zcta_unemployment_rate AS FLOAT64) AS zcta_unemployment_rate,
+  CAST(s.zcta_bachelors_or_higher_rate AS FLOAT64) AS zcta_bachelors_or_higher_rate,
+
+  CAST(s.zcta_match_method AS STRING) AS zcta_match_method,
+
+  CAST(s.census_acs_matched AS BOOL) AS census_acs_matched
+
+FROM
+  `california-hospital-pricing.hospital_pricing.dim_sutter_location` s
+
+LEFT JOIN (
+  SELECT
+    facility_id,
+
+    ANY_VALUE(mrf_location_name) AS mrf_location_name,
+    ANY_VALUE(mrf_hospital_name) AS mrf_hospital_name,
+    ANY_VALUE(mrf_address) AS mrf_address,
+    ANY_VALUE(mrf_license_number) AS mrf_license_number,
+    ANY_VALUE(mrf_type_2_npi) AS mrf_type_2_npi,
+    ANY_VALUE(mrf_last_updated_on) AS mrf_last_updated_on,
+    ANY_VALUE(mrf_version) AS mrf_version
+
+  FROM
+    `california-hospital-pricing.hospital_pricing.fact_sutter_negotiated_price`
+
+  GROUP BY
+    facility_id
+) m
+
 USING (facility_id);
